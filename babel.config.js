a73859cf5b42d5a325054b6263a08f405bbe33ac
pipe-mv <pipe.m.v@hotmail.com>
2026-09-01 12:29:38 +1000

    Upgrade NativeWind to 4

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
     plugins: [
-      ['nativewind/babel'],
       [
         'module:react-native-dotenv',
         {
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
-// metro.config.js
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config')
+const { withNativeWind } = require('nativewind/metro')
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
 
-module.exports = config;
+module.exports = withNativeWind(config, { input: './global.css' })
